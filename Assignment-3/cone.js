6d093eeb370db1615ef3e2b007c600bb01b83379
Scott Hawley <iamultima0@gmail.com>
2021-01-21 17:38:22 -0800

    updated attempts at solution

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -4,6 +4,7 @@ var ConeJS = null;
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
     gl = WebGLUtils.setupWebGL( canvas );
+	
 	//create stringname variables
 	var vertShader = "Cone-vertex-shader";
 	var fragShader = "Cone-fragment-shader";
@@ -12,19 +13,22 @@ function init() {
         alert("Unable to setup WebGL");
         return;
     }
-	//gl.useProgram();
 
 	//set cone variable
-	ConeJS = Cone(gl, 8, vertShader, fragShader);
+	ConeJS = new Cone(gl, 8, vertShader, fragShader);
 	
     gl.clearColor( 0.0, 1.0, 0.0, 1.0 );
+	gl.enable( gl.DEPTH_TEST );
 
     render();
 }
 
 function render() {
-	gl.clear( gl.COLOR_BUFFER_BIT );
-    ConeJS.render();
+	gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+    
+	ConeJS.render();
+	
+	requestAnimationFrame( render );
 }
 
 window.onload = init;
